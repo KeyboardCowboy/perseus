4ed7ce7819b1793775376234ff8b558d9ac53f9f
Chris Albrecht <chris.albrecht@nrel.gov>
2013-08-30 12:51:35 -0600

    renaming settings file

---
+++ default.perseus.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @file
+ * Perseus settings file.
+ *
+ * 1. Copy this file to your /[yoursite]/settings/perseus.php
+ * 2. Ensure it is readable only by your webserver.
+ */
+/**
+ * Define system variables.
+ */
+$vars = array();
+
+/**
+ * Configure databases.
+ */
+$db['dbname'] = array(
+  'host' => 'localhost',
+  'user' => '',
+  'pass' => '',
+);
